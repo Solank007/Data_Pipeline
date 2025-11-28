3e8145b061fc64c9ca9865b67b369d0799d16fea
Sahil <160374409+Solank007@users.noreply.github.com>
2025-11-28 13:07:40 +0530

    Update reddit_dag.py

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -21,7 +21,7 @@ file_postfix = datetime.now().strftime("%Y%m%d")
 dag = DAG(
     dag_id='etl_reddit_pipeline',
     default_args=default_args,
-    schedule='@daily',
+    schedule_interval='@daily',
     catchup=False,
     tags=['reddit', 'etl', 'pipeline']
 )
